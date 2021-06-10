e5ab3b8a57d1afffba54b1b4589b7c990a8a6544
Stephen Ikani <StephenIkani@globusbank.com>
2021-06-10 13:33:56 +0100

    Created a Node Js Server

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Welcome");
+/*
+* Primary file for the API
+*
+*
+*
+*/
+//console.log("Welcome");
+const http=require('http')
+
+//The server should respond to all request in a string
+ var server= http.createServer(function(req, res){
+    res.end('Hello World\n');
+ })
+
+//Start the server, and have it listened on port 3000
+server.listen(3000,function(){
+    console.log("The Server Is Listening On Port 3000")
+})
