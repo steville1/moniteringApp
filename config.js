cba473fc9c1551fbedd576b0114ef57d1995a84e
Stephen Ikani <StephenIkani@globusbank.com>
2021-06-12 04:03:58 +0100

    Added Configuration file, ability to return Json object, set Routing

---
+++ config.js
@@ -0,0 +1,30 @@
+//NODE_ENV=staging node index.js
+
+//Containers for all the environments
+var environments = {
+
+
+};
+//Staging default environments
+
+environments.staging ={
+	'port' : 3000,
+	'envName' : 'staging'
+
+};
+
+//Production environments
+environments.production ={
+	'port' : 5000,
+	'envName' : 'staging'
+
+};
+
+//Determine which environment was passed as a command line arguement
+var currentEnvironment = typeof(process.env.NODE_ENV) == 'string' ? process.env.NODE_ENV.toLowerCase():'';
+
+//Check that the current environment is one of the environments above, if not, default to staging
+var environmentsToExport =typeof(environments[currentEnvironment]) == 'object' ? environments[currentEnvironments] : environments.staging;
+
+//Export the module
+module.exports = environmentsToExport;
--- index.js
+++ index.js
@@ -15,13 +15,77 @@ const url=require('url');
     //Get The Path
     var path=parsedUrl.pathname;
     var trimmedpath=path.replace(/^\/+|\/+$/g,'');
+    //Get the query string
+    var queryStringObject=parsedUrl.query;
+
+    //Get The Http Method
+    var method=req.method.toLowerCase();
+    var headers=req.headers;
+    //Get the payload if any
+    var decoder=new StringDecoder('utf-8');
+    var buffer='';
+    req.on('data', function(data){
+        buffer += decoder.write(data);
+    });
+    req.on('end', function(){
+        buffer += decoder.end();
+        res.end('Hello World\n');
+        //Log The Request Path
+      //Choose the handler this request should go to
+        var choosenHandler = typeof(router[trimmedPath])!== 'undefined' ? router[trimmedPath]:handlers.notFound;
+
+        var data= {
+            'trimmedPath':trimmedPath,
+            'queryStringObject':queryStringObject,
+            'method':method,
+            'headers':headers,
+            'payload':buffer
+
+        };
+
+        //Route the request to the handler specified in the router
+        choosenHandler(data, function(statusCode, payload){
+            //Use the status code called back by the handler
+            statusCode = typeof(statusCode)=='number' ? statusCode : 200;
+            //Use the payload called back by the handler or default to an empty object
+            payload = typeof(payload) == 'object' ? payload : {};
+            //Convert the payload to a string
+            var payloadString = JSON.stringify(payload);
+            //Return the response
+            res.setHeader('Content-Type','application/json');
+            res.writeHead(statusCode);
+            res.end(payloadString);
+
+        });
+
+    });
+
+
+    });
     //Send The Response
-    res.end('Hello World\n');
-    //Log The Request Path
-    console.log('Request Recieved In This Part',+ trimmedpath);
- })
+   // console.log("Request Recieved With This Header" + headers);
+   
 
 //Start the server, and have it listened on port 3000
-server.listen(3002,function(){
-    console.log("The Server Is Listening On Port 3000")
+server.listen(3000,function(){
+    console.log("The Server Is Listening On Port 3000");
 });
+var handlers={};
+
+//Sample Handler
+handlers.sample = function(data, callback){
+	//CallBack a http status code, and a payload object
+	callback(406,{'name':'sample handler'});
+};
+
+//Not Found Handler
+handlers.notFound = function(data, callback){
+	callback(404);
+};
+
+//Define a request router
+var router={
+	sample:handlers.sample
+}
+ 
+
